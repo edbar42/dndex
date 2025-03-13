2039664bc51c52583b5ac7d7937f887df75ad286
Eduardo Barroso <dev@edbar.xyz>
2025-03-13 19:56:01 -0300

    add mix start task

---
+++ lib/dndex.ex
@@ -0,0 +1,18 @@
+defmodule Dndex do
+  @moduledoc """
+  Documentation for `Dndex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dndex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dndex_test.exs
@@ -0,0 +1,8 @@
+defmodule DndexTest do
+  use ExUnit.Case
+  doctest Dndex
+
+  test "greets the world" do
+    assert Dndex.hello() == :world
+  end
+end
